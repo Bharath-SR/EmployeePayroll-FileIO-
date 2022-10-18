963419a6bdcb79abaef49d0112a667027df1c7ce
Bharath-SR <bharath.s0941@gmail.com>
2022-10-18 07:40:29 +0530

    Started Employee Payroll

---
+++ FileIo/src/com/bridgeLabz/FileIO/EmployeePayrolDataService.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz.FileIO;
+
+/*
+ * Started Employee Payroll Service
+ * 
+ */
+public class EmployeePayrolDataService {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Employee PayRole Service");
+	    }
+}
---
+++ FileIo/src/com/bridgeLabz/FileIO/EmployeePayrollData.java
@@ -0,0 +1,5 @@
+package com.bridgeLabz.FileIO;
+
+public class EmployeePayrollData {
+
+}
